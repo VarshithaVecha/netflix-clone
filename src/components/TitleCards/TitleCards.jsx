3dde7bec5b215e7a5a677a8efec0f6dd5b6882bd
VarshithaVecha <157295649+VarshithaVecha@users.noreply.github.com>
2025-05-15 17:16:31 +0530

    Updated code after changes

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,5 +1,5 @@
 import React,{useEffect,useRef, useState} from 'react'
-import './TitleCards.css'
+import './Titlecards.css'
 import { Link } from 'react-router-dom';
 //import cards_data from '../../assets/cards/Cards_data'
 
